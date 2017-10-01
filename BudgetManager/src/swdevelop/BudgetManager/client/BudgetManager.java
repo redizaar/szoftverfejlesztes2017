da0340ad7a50df2e54515e835818fd7c7ce13828
redizaar <pwnpwn90@gmail.com>
2017-10-01 12:25:08 +0200

    Add files via upload

--- BudgetManager/src/swdevelop/BudgetManager/client/BudgetManager.java
+++ BudgetManager/src/swdevelop/BudgetManager/client/BudgetManager.java
@@ -21,7 +21,6 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-import swdevelop.BudgetManager.client.MainMenu.ComponentHandler;
 import swdevelop.BudgetManager.client.User.User;
 
 /**
@@ -54,9 +53,11 @@ public class BudgetManager implements EntryPoint {
 		final Label passwordLabel = new Label();
 		passwordLabel.setText("Password: ");
 		// We can add style names to widgets
+		userLabel.setStyleName("loginLabel");
+		passwordLabel.setStyleName("loginLabel");
 		
-		loginButton.setStyleName("buttons");
-		registerButton.setStyleName("buttons");
+		loginButton.setStyleName("sendButton");
+		registerButton.setStyleName("sendButton");
 		final VerticalPanel verticalPanel = new VerticalPanel();
 		HorizontalPanel horizontal1 = new HorizontalPanel();
 		horizontal1.add(userLabel);
@@ -66,17 +67,19 @@ public class BudgetManager implements EntryPoint {
 		horizontal2.add(passwordTextBox);
 		verticalPanel.add(horizontal1);
 		verticalPanel.add(horizontal2);
-		verticalPanel.add(loginButton);
 		
 		final Label registerLabel= new Label();
-		registerLabel.setText("Doesn't have an account ?");		
+		registerLabel.setText("Don't have an account ?");		
 		
-		verticalPanel.add(registerLabel);
-		verticalPanel.add(registerButton);
 		verticalPanel.getElement().setAttribute("align", "center");
 		// Use RootPanel.get() to get the entire body element
 		RootPanel.get().add(verticalPanel);
-
+		final VerticalPanel vp=new VerticalPanel();
+		vp.add(loginButton);
+		vp.add(registerLabel);
+		vp.add(registerButton);
+		vp.getElement().setAttribute("align", "center");
+		RootPanel.get().add(vp);
 		// Focus the cursor on the name field when the app loads
 		usernameTextBox.setFocus(true);
 
@@ -99,7 +102,9 @@ public class BudgetManager implements EntryPoint {
 									{
 										user=new User(usernameTextBox.getText(),result);
 										verticalPanel.setVisible(false);
-										new ComponentHandler();
+										vp.setVisible(false);
+										Window.alert("Sikeres bejelentkezes!");
+										//new ComponentHandler();
 									}
 									else
 									{
@@ -126,11 +131,11 @@ public class BudgetManager implements EntryPoint {
 				registerLabel.setVisible(false);
 				registerButton.setVisible(false);
 				userLabel.setText("Username: ");
-				Label passwordAgainLabel = new Label("  Type in your password again: ");
+				final Label passwordAgainLabel = new Label("  Type in your password again: ");
 				final PasswordTextBox passwordAgain = new PasswordTextBox();
 				horizontal2.add(passwordAgainLabel);
 				horizontal2.add(passwordAgain);
-				Button newRegisertButton=new Button("Register");
+				final Button newRegisertButton=new Button("Register");
 				newRegisertButton.setStyleName("buttons");
 				verticalPanel.add(newRegisertButton);
 				newRegisertButton.addClickHandler(new ClickHandler() {
@@ -156,7 +161,15 @@ public class BudgetManager implements EntryPoint {
 											public void onSuccess(Boolean result) {
 												if(result)
 												{
-													Window.alert("Sikeresen regisztrálva!");
+													Window.alert("Sikeresen regisztralva!");
+													loginButton.setVisible(true);
+													registerLabel.setVisible(true);
+													registerButton.setVisible(true);
+													newRegisertButton.setVisible(false);
+													passwordAgainLabel.setVisible(false);
+													passwordAgain.setVisible(false);
+													passwordTextBox.setText("");
+													usernameTextBox.setText("");
 												}
 												else
 												{
